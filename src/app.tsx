5b5964d9d28eec6b9ee3ebe1f57f3aa58ea98769
Enrico Secco <enricosecco@edu.unifil.br>
2024-09-13 15:56:29 -0300

    feat: add react query for fetching

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
 import { CreateGoal } from './components/create-goal'
-import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
 import { Dialog } from './components/ui/dialog'
+import { getSummary } from './http/get-summary'
+import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+    //Create an intern cache for the result of this query for 60 seconds
+    //i.e if my application need this data on a 60 seconds interval, it won't make a new request, it will just return the response on cache
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
